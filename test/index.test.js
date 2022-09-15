88eb57a16499d815e9232550bebf3ca195270fff
shihong <shih01@servyou.com.cn>
2022-09-15 14:25:28 +0800

    test(core): 调整 test 目录

--- test/index.test.js
+++ test/index.test.js
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom'
 import { render, fireEvent, waitFor, screen } from '@testing-library/react'
 import { act } from 'react-dom/test-utils'
 import '@testing-library/jest-dom/extend-expect'
-import { imc, create } from './index'
+import { imc, create } from '@rdecojs/core'
 
 test('imc and create api', async () => {
   create(function helloWorld() {
